7d8b0e80bf6fc26878481fb53910f62ebf4e9912
lolcat-max <steamlolcat0@gmail.com>
2025-11-17 08:22:31 +1100

    previously for sha256

--- Qdriver.ino
+++ Qdriver.ino
@@ -12,7 +12,7 @@ int outputValue = 0;
 
 String inputString = "";
 const int analogThreshold = 800; //sensitivity to black phosphorus (requires configuration) 
-int work = 10;
+int work = 20000;
 
 // Mining variables
 SHA256 sha256;
@@ -32,7 +32,7 @@ void loop() {
   // Record the start time in microseconds
   unsigned long startTime = micros();
 
-  mineSHA256();
+  processing();
 
   // Record the end time in microseconds
   unsigned long endTime = micros();
@@ -45,41 +45,19 @@ void loop() {
 
 }
 
-void mineSHA256() {
+void processing() {
   float sensorValue = analogRead(A0);
   for (int i = 0; i < work; i++) {
     if (sensorValue > analogThreshold) { //sensitivity to black phosphorus (requires configuration) 
       break; //early stopping
     }
-    String input = String(prefix) + String(nonce + i);
-
-    sha256.reset();
-    sha256.update(input.c_str(), input.length());
-    
-    uint8_t hash[32];
-    sha256.finalize(hash, 32);
-    
-    String hashStr = "";
-    for (int j = 0; j < 32; j++) {
-      if (hash[j] < 16) hashStr += "0";
-      hashStr += String(hash[j], HEX);
-    }
-    
-    bool found = true;
-    for (int j = 0; j < difficulty; j++) {
-      if (hashStr.charAt(j) != '0') {
-        found = false;
-        break;
-      }
-    }
-    
+    bool found = false;
     if (found) {
       digitalWrite(13, HIGH);
-      Serial.println("\n*** BLOCK FOUND ***");
+      Serial.println("\n*** Processing Done!***");
       Serial.print("Nonce: ");
       Serial.println(nonce + i);
-      Serial.print("Hash: ");
-      Serial.println(hashStr);
+
       Serial.println("*******************\n");
       nonce += work;
       delay(2); //duration of barium titanate activation
